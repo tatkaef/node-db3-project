67716dc17e31cf58984f570de7856636e5e1822a
Tatiana Efimova <tatka_ef@mail.ru>
2020-12-10 01:56:13 -0800

    MVP

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,32 @@
 // scheme-model
+
+const db = require("../../data/db-config.js");
+
+module.exports = {
+
+    find() {
+        return db('schemes')
+    },
+    
+    findById(id) {
+        return db('schemes').where({ id }).first();
+    },
+
+    findSteps(id) {
+        return db('steps').where({ scheme_id: id })
+    },
+
+    add(user) {
+        return db('schemes').insert(user, 'id')
+    },
+    
+    update(changes, id) {
+        return db('schemes').where({id}).update(changes)
+    },
+
+    remove(id) {
+        return db('schemes').where({id}).del()
+    },  
+    
+    
+  }
